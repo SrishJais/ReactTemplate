c31e98feff8590c793be21f5d6530d1ea4021ff5
Srishti Jaiswal <84665176+SrishJais@users.noreply.github.com>
2023-10-20 13:24:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound ";
+
+function App() {
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
